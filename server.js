f7aeec696dd78d816a603d42295464b5567e0a54
Dewaa <arisurya228@gmail.com>
2025-12-03 01:34:06 +0800

    fixing cors error

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://vandal-projects.vercel.app/"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, // kalau pakai cookie/session
+  })
+);
 
 import registerRoutes from "./api/routes/registerRoutes.js";
 import watchlistRoutes from "./api/routes/watchlistRoutes.js";
